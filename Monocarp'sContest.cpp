2de17d4aba91c75de7315f3d387e1905016c5ea1
Pavithra0605 <pavicode6@gmail.com>
2026-09-10 11:20:48 +0530

    Update Monocarp'sContest.cpp

--- Monocarp'sContest.cpp
+++ Monocarp'sContest.cpp
@@ -7,7 +7,8 @@
 using namespace std;
 #define ll long long int
 void solve(){
- int N;cin>>N;
+ int N;
+ cin>>N;
    vector<int> arr(N);
    int oc=0;
    for(int i=0;i<N;i++){
@@ -18,18 +19,15 @@ void solve(){
    if(z<2){
     cout<<-1<<"\n"; return;
    }
-   //there are atleast 2 rows
-   int ans=0;
-   if(arr[0]) ans++;
-   if(arr[N-1]) ans++;
-   cout<<ans<<endl;
+   int fin=0;
+   if(arr[0]) in++;
+   if(arr[N-1]) fin++;
+   cout<<fin<<endl;
 }
-
  int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int T = 1;
     cin >> T;
-    
     while(T--) solve();
 }
